aa649addd4cc33cfede3bdc97ee671fd016b9ced
ruf <bob.wolfe@gmail.com>
2016-09-21 10:44:39 +0800

    我试图用两个set代替了一个map。set f 里面存放还没有分配出去的房间号。这样代码貌似又简洁了一些。

--- requests/001/answer.py
+++ requests/001/answer.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python
 
-m = {101:'f',102:'f',103:'f',104:'f',105:'f',106:'f',107:'f',108:'f',109:'f',110:'f',111:'f',112:'f'}
-m = {k:'f' for k in range(101,113)}
+m = {k for k in range(101,113)}
+f = {k for k in range(101,113)}
+
 while True:
  cio = input('What can I help you (1-check in, 2-check out) ? ')
  if cio == 1:
-  f = False
-  for k in m:
-   if m[k] == 'f':
-    f = True
-    print 'Your room number is %i, this is your room key.' % k
-    m[k] = 'u'
-    break
-  if f == False:
+  if len(f) > 0:
+   rn = list(f)[0]
+   f.remove(rn)
+   print 'Your room number is %i, this is your room key.' % rn
+  else:
    print "Sorry we don't have free room now."
  if cio == 2:
-  rn = input('What is your rom number ? ') 
+  rn = input('What is your room number ? ') 
   if rn in m:
-   if m[rn] == 'u':
-    m[rn] = 'f'
+   if not rn in f:
+    f.add(rn)
     print 'Thank you, good bye.'
    else:
     print 'This room is not allocated yet.'
   else:
    print 'Sorry the room number is wrong.'
- n = 0
- for k in m:
-  if m[k] == 'f':
-   n += 1
- if n > 0:
-  print 'There are %i free rooms.' % n
+ if len(f) > 0:
+  print 'There are %i free rooms.' % len(f)
  else:
-  print 'No free rooms'
- 
+  print 'No free rooms.'
