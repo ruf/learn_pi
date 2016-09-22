77b011b9d1dfafda4791df71189c85c482735284
ruf <bob.wolfe@gmail.com>
2016-09-22 14:08:06 +0800

    add color.sh

---
+++ requests/009/color.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# generates an 8 bit color table (256 colors) for
+# reference purposes, using the \033[48;5;${val}m
+# ANSI CSI+SGR (see "ANSI Code" on Wikipedia)
+#
+# It is also possible (in these newfangled modern times) 
+# to use a full 256 colors in most consoles. The color 
+# table below can be used to determine the code for each
+# color by adding the column and row number. In order to
+# make use of these codes, we use the syntax 33[38;5;#m
+# for the foreground (text) and 33[48;5;#m for the back-
+# ground, or in a single statement like 33[38;5;#;48;5;#m
+# to set both at once, where # is an 8-bit (0-255) color code.
+
+echo -en "\n   +  "
+for i in {0..35}; do
+  printf "%2b " $i
+done
+
+printf "\n\n %3b  " 0
+for i in {0..15}; do
+  echo -en "\033[48;5;${i}m  \033[m "
+done
+
+#for i in 16 52 88 124 160 196 232; do
+for i in {0..6}; do
+  let "i = i*36 +16"
+  printf "\n\n %3b  " $i
+  for j in {0..35}; do
+    let "val = i+j"
+    echo -en "\033[48;5;${val}m  \033[m "
+  done
+done
+
+echo -e "\n"
